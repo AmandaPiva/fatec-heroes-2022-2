026027112c867400732c6b497f8cdea3d9b868c4
amanda.piva <amanda.piva01@fatec.sp.gov.br>
2022-09-27 11:01:37 -0300

    Comecando o heroes

--- my-app2/src/app/app.component.ts
+++ my-app2/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app2';
+export class AppComponent { //class do decorator (componente)
+  title = 'Tuor of Heroes';
 }
---
+++ my-app2/src/app/hero.ts
@@ -0,0 +1,6 @@
+//estrutura para guardar um heroi
+//Todos os dados desse heroi
+export interface Hero {
+    id: number; //tipo desses dados pré definidos
+    name: string;
+  }
