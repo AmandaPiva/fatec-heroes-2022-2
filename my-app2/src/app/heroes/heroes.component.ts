e45eee0aa180497b56badaf06b34b3de425bbcaa
amanda.piva <amanda.piva01@fatec.sp.gov.br>
2022-10-04 10:54:53 -0300

    Finalização do projeto

--- my-app2/src/app/heroes/heroes.component.ts
+++ my-app2/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,16 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
   
-  hero: Hero = {  //interface
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+ 
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 }
