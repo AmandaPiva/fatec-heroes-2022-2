4f444d3d9c069c432e781afb87c397fa2ff206b3
amanda.piva <amanda.piva01@fatec.sp.gov.br>
2022-10-25 10:54:46 -0300

    Novos conceitos: Services, Injectable e criação desses services, constructor e codigo assincrono

--- my-app2/src/app/heroes/heroes.component.ts
+++ my-app2/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,15 +9,22 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  
-  heroes = HEROES;
+  //
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+  
 
-  constructor() { }
+  constructor(private heroService: HeroService) {}  //Injetando 
+
+  getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+} 
 
   ngOnInit(): void {
+    this.getHeroes();
   }
-
+  //
  
 onSelect(hero: Hero): void {
   this.selectedHero = hero;
